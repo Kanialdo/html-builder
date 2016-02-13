c21cce6c5fb90ac560363a633579ac2138e202cf
Krystian Kaniowski <kanialdo@gmail.com>
2016-02-13 23:28:01 +0100

    Add some html builder commands to build simple web page based on divs.

--- src/pl/krystiankaniowski/htmlbuilder/HtmlBuilder.java
+++ src/pl/krystiankaniowski/htmlbuilder/HtmlBuilder.java
@@ -1,4 +1,35 @@
 package pl.krystiankaniowski.htmlbuilder;
 
+import pl.krystiankaniowski.htmlbuilder.html.HtmlPacker;
+
 public class HtmlBuilder {
+
+    private String html;
+    private StringBuilder head;
+    private StringBuilder body;
+
+    public HtmlBuilder() {
+
+        head = new StringBuilder();
+        body = new StringBuilder();
+
+    }
+
+    public void addSection(String header, String content) {
+        body.append(HtmlPacker.div(HtmlPacker.h1(header) + content));
+    }
+
+    public void addCustom(String content) {
+        body.append(content);
+    }
+
+    public HtmlBuilder build() {
+        html = "<!DOCTYPE html>" + HtmlPacker.pack("html", HtmlPacker.pack("head", head.toString()) + HtmlPacker.pack("body", body.toString()));
+        return this;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
 }
--- test/HtmlPackerTest.java
+++ test/HtmlPackerTest.java
@@ -7,11 +7,9 @@ import java.util.List;
 
 public class HtmlPackerTest {
 
-    HtmlPacker packer = new HtmlPacker();
-
     @Test
     public void testDiv() throws Exception {
-        Assert.assertEquals("<div>abc</div>", packer.div("abc"));
+        Assert.assertEquals("<div>abc</div>", HtmlPacker.div("abc"));
     }
 
     @Test
@@ -22,20 +20,21 @@ public class HtmlPackerTest {
         list.add("b");
         list.add("c");
 
-        Assert.assertEquals("<ul><li>a</li><li>b</li><li>c</li></ul>", packer.ul(list));
+        Assert.assertEquals("<ul><li>a</li><li>b</li><li>c</li></ul>", HtmlPacker.ul(list));
 
     }
 
     @Test
     public void testHeaders() throws Exception {
-        Assert.assertEquals("<h1>abc</h1>", packer.h1("abc"));
-        Assert.assertEquals("<h2>abc</h2>", packer.h2("abc"));
-        Assert.assertEquals("<h3>abc</h3>", packer.h3("abc"));
+        Assert.assertEquals("<h1>abc</h1>", HtmlPacker.h1("abc"));
+        Assert.assertEquals("<h2>abc</h2>", HtmlPacker.h2("abc"));
+        Assert.assertEquals("<h3>abc</h3>", HtmlPacker.h3("abc"));
     }
 
     @Test
     public void testPack() throws Exception {
-
+        Assert.assertEquals("<p>abc</p>", HtmlPacker.p("abc"));
+        Assert.assertEquals("<a href='http://google.com'>Google</a>", HtmlPacker.a("http://google.com", "Google"));
     }
 
 }
